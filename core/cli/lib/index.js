a6d2f71a04a1bc0337403c23124a2b91b016fc41
祁嘉俊 <1481700559@qq.com>
2025-03-27 10:55:07 +0800

    feat: 添加node版本检查、root启动、用户主目录

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,15 +1,51 @@
-'use strict'
+"use strict"
 
 module.exports = core
 
-const pkg = require('../package.json')
-const log = require('@minorn-cli/log')
+const semver = require("semver")
+const log = require("@minorn-cli/log")
+const colors = require("colors")
+const userHome = require("user-home")
+const pathExists = require("path-exists").sync
+const pkg = require("../package.json")
+const constant = require("./const")
 
 function core() {
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+    checkRoot()
+    checkUserHome()
+  } catch (e) {
+    log.error(e.message)
+  }
 }
 
 // 检查版本号
 function checkPkgVersion() {
-  log.notice('cli', pkg.version)
+  log.notice("cli", pkg.version)
+}
+
+function checkNodeVersion() {
+  const currentVersion = process.version
+  const lowestVersion = constant.LOWEST_NODE_VERSION
+  if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(
+      colors.red(
+        `minorn-cli 需要安装 v${lowestVersion} 以上版本的 Node.js,当前版本是${currentVersion}`
+      )
+    )
+  }
+}
+
+// 检查 root 账户启动，如果是 root 降级处理(mac)
+function checkRoot() {
+  require("root-check")()
+}
+
+// 判断用户主目录是否存在
+function checkUserHome() {
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red("当前登录用户主目录不存在"))
+  }
 }
