c791ac215ba7a1d59934427369d71fee69f2fc5d
minorn <1481700559@qq.com>
2025-03-31 21:46:09 +0800

    feat； 清空用户目录force开发

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,24 +1,26 @@
-"use strict"
+'use strict'
 
-const fs = require("fs")
-const inquirer = require("inquirer")
-const fse = require("fs-extra")
-const Command = require("@minorn-cli/command")
-const log = require("@minorn-cli/log")
+const fs = require('fs')
+const inquirer = require('inquirer')
+const fse = require('fs-extra')
+const Command = require('@minorn-cli/command')
+const log = require('@minorn-cli/log')
 
 class InitCommand extends Command {
   constructor(argv) {
     super(argv)
   }
   init() {
-    this.projectName = this._argv[0] || ""
+    this.projectName = this._argv[0] || ''
     this.force = !!this._cmd.force
-    log.verbose("projectName:", this.projectName, "force:", this.force)
+    log.verbose('projectName:', this.projectName, 'force:', this.force)
   }
   async exec() {
     try {
       // 准备
-      await this.prepare()
+      const ret = await this.prepare()
+      if (ret) {
+      }
       // 下载模板
       // 安装模板
     } catch (e) {
@@ -31,20 +33,32 @@ class InitCommand extends Command {
     const localPath = process.cwd()
 
     if (!this.IsDirEmpty(localPath)) {
-      const { ifContinue } = await inquirer.prompt({
-        type: "confirm",
-        name: "ifContinue",
-        message: "当前文件夹不为空，是否继续创建项目？",
-        default: false,
-      })
-      if (!ifContinue) {
-        return
+      let ifContinue = false
+      if (!this.force) {
+        ifContinue = (
+          await inquirer.prompt({
+            type: 'confirm',
+            name: 'ifContinue',
+            message: '当前文件夹不为空，是否继续创建项目？',
+            default: false,
+          })
+        ).ifContinue
+        if (!ifContinue) {
+          return
+        }
       }
-      if (ifContinue) {
+      if (ifContinue || this.force) {
         // 强制更新
-        fse.emptyDirSync(localPath)
+        const { confirmDelete } = await inquirer.prompt({
+          type: 'confirm',
+          name: 'confirmDelete',
+          message: '是否确认清空当前目录？',
+          default: false,
+        })
+        if (confirmDelete) {
+          fse.emptyDirSync(localPath)
+        }
       }
-      console.log("answer", ifContinue)
     }
     // 是否是 force
     // 组件/项目
@@ -54,7 +68,7 @@ class InitCommand extends Command {
   IsDirEmpty(localPath) {
     let fileList = fs.readdirSync(localPath)
     fileList = fileList.filter(
-      (file) => !file.startsWith(".") && !["node_modules"].includes(file)
+      (file) => !file.startsWith('.') && !['node_modules'].includes(file)
     )
     return !fileList || fileList.length <= 0
   }
